97e9a427d6cc8c453c3f77ea98ab286449530c08
zyx <3064662835@qq.com>
2019-02-24 18:11:08 +0800

    创建NetworkingModule组件文件

---
+++ Example/Pods/Target Support Files/NetworkingModule/NetworkingModule-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NetworkingModule : NSObject
+@end
+@implementation PodsDummy_NetworkingModule
+@end
---
+++ Example/Pods/Target Support Files/Pods-NetworkingModule_Tests/Pods-NetworkingModule_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NetworkingModule_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_NetworkingModule_Tests
+@end
